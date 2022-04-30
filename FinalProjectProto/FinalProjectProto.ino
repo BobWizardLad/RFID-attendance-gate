204ff63f033076094aefce286e2d9269bc354688
Kyle Orman <61096772+kyleorman@users.noreply.github.com>
2022-04-29 22:33:14 -0500

    Added ability to read info from card
    No MQTT or oled functionality created yet

--- FinalProjectProto/FinalProjectProto.ino
+++ FinalProjectProto/FinalProjectProto.ino
@@ -4,11 +4,26 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 11
+#define RST_PIN 6
+ 
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Instance of the class
+
+MFRC522::MIFARE_Key key; 
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+// Init array that will store new NUID 
+byte kyleUID[4];
+
 
 // Define MQTT topics
 #define TOPIC "attendance"
 #define USERNAME "kaorman"
-#define SERVO_PIN 9
 #define SCREEN_WIDTH 128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define RED_LED 1
@@ -20,6 +35,8 @@ Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 // Set SSID and network password
 char ssid[] = SSID;
 char password[] = PASSKEY;
+
+// char studentID;
 
 //Status
 int status = WL_IDLE_STATUS;
@@ -54,11 +71,23 @@ void greenOn(); // Green LED ON
 void greenOff(); // Green LED OFF
 void redOn(); // Red LED ON
 void redOff(); // Red LED OFF
+void printHex(); // Helper routine to dump a byte array as hex values to Serial
+void studentIdentifier();
 
 void setup() {
   // put your setup code here, to run once:
+  // Set kyle's UID card number in HEX
+  kyleUID[0] = 0x53;
+  kyleUID[1] = 0x1C;
+  kyleUID[2] = 0xF2;
+  kyleUID[3] = 0x31;
+
   // Serial connection set to 115200 BAUD
   Serial.begin(115200);
+
+  SPI.begin(); // Init SPI bus for rfid
+  mfrc522.PCD_Init(); // Init MFRC522 card reader
+
   // Connect to local wifi
   setup_wifi();
   // Initial connection to mqtt server
@@ -149,6 +178,7 @@ void loop() {
     // Recieving RFID attendance operation
     case RECORD:
       rfidTask();
+      //studentIdentifier();
       lcdTask();
     break;
     // Sending data to MQTT
@@ -168,7 +198,93 @@ void lcdTask() {
 }
 
 void rfidTask(){
+  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
 
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.println(F("**Student Detected:**"));
+
+  //mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
+
+Serial.print(F("ID: "));
+
+  byte buffer1[18];
+
+  block = 4;
+  len = 18;
+
+  //------------------------------------------- GET ID
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  //PRINT ID
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    if (buffer1[i] != 32)
+    {
+      Serial.write(buffer1[i]);
+    }
+  }
+  Serial.print(" ");
+
+  //---------------------------------------- GET LAST NAME
+
+  byte buffer2[18];
+  block = 1;
+
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer2, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  Serial.println();
+
+  Serial.print(F("Last Name: "));
+
+  //PRINT LAST NAME
+  for (uint8_t i = 0; i < 16; i++) {
+    Serial.write(buffer2[i] );
+  }
+  delay(1000); //change value if you want to read cards faster
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+  Serial.println();
 }
 
 void greenOn(){
@@ -186,3 +302,41 @@ void redOn(){
 void redOff(){
   digitalWrite(RED_LED, LOW);
 }
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+// void stuID(byte *buffer, byte bufferSize) {
+//   bool isKyle = false;
+//   for (byte i = 0; i < bufferSize; i++) {
+//     if(buffer[i] != kyleUID[i]){
+//       isKyle = false;
+//     }
+//     else{
+//       isKyle = true;
+//     }
+//   }
+//   if(isKyle){
+//     studentID = '010919454';
+//     Serial.print("Student ID: " + studentID);
+//     Serial.println();
+//   }
+// }
+
+// void studentIdentifier() {
+//     char studentID;
+//     if(rfid.uid.uidByte[0] == kyleUID[0] || 
+//       rfid.uid.uidByte[1] == kyleUID[1] || 
+//       rfid.uid.uidByte[2] == kyleUID[2] || 
+//       rfid.uid.uidByte[3] == kyleUID[3] ){
+//         studentID = '010919454';
+
+//     }
+//     else {studentID = 'Not Found';}
+//     Serial.print("Student ID: " + studentID);
+//     Serial.println();
+// }
